b1e8480419ecbe635b3459ff03bfd75c5db8d249
Kyle <nzl.kyle@gmail.com>
2023-04-12 17:35:03 +1200

    Fixed imagedata tag name in VML mapping causing OpenXmlUnknownElement

--- Doc/WordprocessingMLMapping/VMLPictureMapping.cs
+++ Doc/WordprocessingMLMapping/VMLPictureMapping.cs
@@ -26,7 +26,7 @@ namespace b2xtranslator.WordprocessingMLMapping
         {
             this._targetPart = targetPart;
             this._olePreview = olePreview;
-            this._imageData = this._nodeFactory.CreateElement("v", "imageData", OpenXmlNamespaces.VectorML);
+            this._imageData = this._nodeFactory.CreateElement("v", "imagedata", OpenXmlNamespaces.VectorML);
         }
 
         public void Apply(PictureDescriptor pict)
